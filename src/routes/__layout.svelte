68ddbb610ee33c4f9a5d173efb924e45d9525298
gregO <greg.owens@me.com>
2021-08-10 15:03:23 +1000

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/banks">Banks</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+
+// index to show default dashboard
+
+
+
+
+<svelte:head>
+	<title>pifl - simple money</title>
+</svelte:head>
+
+<h1>Welcome to pifl - the simplest money app!</h1>
+<p>Take hold of your cash!</p>
